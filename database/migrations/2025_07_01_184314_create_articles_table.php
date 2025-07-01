7d822f392129e518cdfa6e0cefdb0310ebb4cdd8
Bryan Dela Cruz <137765730+Bryan652@users.noreply.github.com>
2025-07-01 14:50:39 -0400

    Factory, Models, and migrations

--- database/migrations/2025_07_01_184314_create_articles_table.php
+++ database/migrations/2025_07_01_184314_create_articles_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,10 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->foreignId(User::class, 'user_id')->constrained()->cascadeOnDelete();
+            $table->string('postText');
+            $table->string('postImage')->nullable();
+            $table->string('postVideo')->nullable();
             $table->timestamps();
         });
     }
