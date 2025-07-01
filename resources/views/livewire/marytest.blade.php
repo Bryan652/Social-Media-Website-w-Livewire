f1f666c74e3a33b4831bdad04017cbee103e3a33
Bryan Dela Cruz <137765730+Bryan652@users.noreply.github.com>
2025-07-01 14:44:19 -0400

    Addition of tables(pivotTable & article)

--- resources/views/livewire/marytest.blade.php
+++ resources/views/livewire/marytest.blade.php
@@ -3,9 +3,18 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+
 }; ?>
 
 <div>
-    <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">Responsive</button>
+    @php
+        $slides = [
+            ['image' => '/photos/photo-1494253109108-2e30c049369b.jpg'],
+            ['image' => '/photos/photo-1565098772267-60af42b81ef2.jpg'],
+            ['image' => '/photos/photo-1559703248-dcaaec9fab78.jpg'],
+            ['image' => '/photos/photo-1572635148818-ef6fd45eb394.jpg'],
+        ];
+    @endphp
+
+    <x-mary-carousel :slides="$slides" />
 </div>
